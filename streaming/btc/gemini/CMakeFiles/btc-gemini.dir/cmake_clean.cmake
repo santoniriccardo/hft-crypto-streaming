cb74d977148ab94cc42ce167ae8c9eefd4aa14d4
santoniriccardo <santoni.riccardo@gmail.com>
2022-04-05 16:17:45 -0400

    Introduced initial connecting and streaming of asset information for btc from both coinbase + gemini. Need to include more, as well as sequential version

---
+++ streaming/btc/gemini/CMakeFiles/btc-gemini.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/btc-gemini.dir/main.cpp.o"
+  "CMakeFiles/btc-gemini.dir/main.cpp.o.d"
+  "btc-gemini"
+  "btc-gemini.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/btc-gemini.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ streaming/btc/gemini/CMakeFiles/btc-gemini.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for btc-gemini.
+# This may be replaced when dependencies are built.
